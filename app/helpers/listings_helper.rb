8da8df6cc95a0873c0a84741a081f760d00b700b
pyksoft <pyksoft@gmail.com>
2017-10-12 10:30:42 +1100

    Move format_money method from listing helper to application helper

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,7 +1,3 @@
 module ListingsHelper
-    # Takes 10000 cents and output is as $100.00
-    def format_money(cents)
-        money = Money.new(cents, "AUD")
-        money.format
-    end
+    
 end
