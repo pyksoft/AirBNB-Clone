bc1449e50e4fb54a475d5fbfdc51d86f1f987311
pyksoft <pyksoft@gmail.com>
2017-10-12 11:15:32 +1100

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Listing.destroy_all
 
 Listing.create!([
     {
